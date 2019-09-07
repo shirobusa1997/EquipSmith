39028d418c7e17b45e313c7d0513a30987ceb3d9
Mojatto_MBP <daichiandroid1997@gmail.com>
2019-09-08 00:26:00 +0900

    Djangoチュートリアルを進行。

--- equipsmith/equipsmith/settings.py
+++ equipsmith/equipsmith/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'equipsmith_dev.apps.EquipsmithDevConfig',
+    'bootstrap4',                               # PipPackage : django-bootstrap4
 ]
 
 MIDDLEWARE = [
--- equipsmith/equipsmith_dev/views.py
+++ equipsmith/equipsmith_dev/views.py
@@ -2,5 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-	return HttpResponse("ハロー・ワールド.")
+
+def book_list(request):
+	# 書庫の一覧
+	return HttpResponse('書庫の一覧')
+
+
+def bool_edit(request, book_id=None):
+	# 書庫の編集
+	return HttpResponse('書庫の編集')
+
+
+def bool_del(request, book_id):
+	# 書庫の削除
+	return HttpResponse('書庫の削除')
+# def index(request):
+# 	return HttpResponse("ハロー・ワールド.")
