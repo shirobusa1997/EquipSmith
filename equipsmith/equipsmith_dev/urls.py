c65100d2cdb41541de92fe3993192ea3ead6ae26
Mojatto_MBP <daichiandroid1997@gmail.com>
2019-09-09 15:02:14 +0900

    チュートリアルの進行。URLと各Viewファンクションを紐付け。

--- equipsmith/equipsmith_dev/urls.py
+++ equipsmith/equipsmith_dev/urls.py
@@ -6,5 +6,8 @@ from . import views
 app_name = 'equipsmith_dev'
 urlpatterns = [
 	# path('', views.index, name='index'),
-	
+	path('book/', views.book_list, name='book_list'),
+	path('book/add', views.book_edit, name='book_add'),
+	path('book/mod/<int:book_id/', views.book_edit, name='book_mod'),
+	path('book/del/<int:book_id/', views.book_del, name='book_del'),
 ]
