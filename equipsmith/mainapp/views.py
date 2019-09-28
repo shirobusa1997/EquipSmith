471d7d4713418dcf74248c07bdd146d3f35c1cef
Mojatto_MBP <daichiandroid1997@gmail.com>
2019-09-28 13:25:45 +0900

    ヘッダーナビゲーション機能の変更。ユーザ管理機能のヘッダーへの統合。

--- equipsmith/mainapp/views.py
+++ equipsmith/mainapp/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("EQUIP SMITH BLANK PAGE")
+	# return HttpResponse("EQUIP SMITH BLANK PAGE")
+	return redirect("bstest")
 
 
 def bstest(request):
