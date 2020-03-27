f3959a514fbf956e9819d4b849b7d26c2c1cb5ff
Mojatto_MBP <daichiandroid1997@gmail.com>
2020-03-27 23:48:08 +0900

    モデルを更新．
    賃借管理用モデルを追加．

--- docker/mainapp/admin.py
+++ docker/mainapp/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from mainapp.models import Equipments
+from mainapp.models import Equipments, Records
 
 # Register your models here.
 class EquipmentsAdmin(admin.ModelAdmin):
 	list_display = ('id', 'name', 'equip_attribute', 'equip_status', 'registered_date',)
 	list_display_links = ('id', 'name',)
 
-admin.site.register(Equipments, EquipmentsAdmin)
+class RecordsAdmin(admin.ModelAdmin):
+	list_display = ('id', 'equipid', 'recorded_date',)
+	list_display_links = ('id',)
 
+admin.site.register(Equipments, EquipmentsAdmin)
+admin.site.register(Records, RecordsAdmin)
